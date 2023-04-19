20672557902ba7ce64f5a0aba4b7206586f861f3
deaktive <125353682+deaktive@users.noreply.github.com>
2023-04-19 08:30:36 +0700

    Add files via upload

---
+++ ronda.py
@@ -0,0 +1,129 @@
+import time
+import asyncio
+import sys
+import threading
+import telethon
+import re
+from telethon import TelegramClient, events, utils
+from telethon.tl.functions.messages import GetBotCallbackAnswerRequest
+
+api_id = 19426024
+api_hash = '93e64ab1fca196717682f11b879b9214'
+sesi_fil = 'rektvps'
+
+alamat = [
+'/curiuang_940776279704',
+'/curiuang_330041286550',
+'/curiuang_331251158135',
+'/curiuang_670639242838',
+'/curiuang_421325453322',
+'/curiuang_260960577212',
+'/curiuang_851156114456',
+'/curiuang_560668137555',
+'/curiuang_571840135711',
+'/curiuang_581357305752',
+'/curiuang_480577138034',
+'/makan_kudapansuci',
+'/levelupguild',
+]
+
+Grade = [
+'',]
+
+mese = '/masak_minibacon_220'
+thebot = "KampungMaifamxBot"
+mer = 0
+
+def Dexter():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    with TelegramClient(sesi_fil, api_id, api_hash, loop=loop) as clien:
+        clien.loop.run_until_complete(clien.send_message('KampungMaifamxBot', mese))
+
+        @clien.on(events.NewMessage(from_users='KampungMaifamxBot'))
+        async def handler(event):
+            global mer
+            global idMer
+            global gradenum
+
+            if "Berhasil memasak" in event.raw_text:
+                time.sleep(2)
+                await event.respond(mese)
+                return
+
+            if "Energi tidak mencukupi" in event.raw_text:
+                time.sleep(2)
+                await event.respond('restore')
+                return
+
+            if "Energi berhasil" in event.raw_text:
+                time.sleep(2)
+                await event.respond(mese)
+                return
+
+            if "Tidak cukup bahan" in event.raw_text:
+                time.sleep(2)
+                await event.respond('/dapur_restock_MAX_confirm')
+                return
+
+            if "Bahan-bahan direstock" in event.raw_text:
+                time.sleep(2)
+                await event.respond(mese)
+                return
+
+            if "Kamu tidak bisa memasak" in event.raw_text:
+                mer += 0
+                time.sleep(2)
+                await event.respond(''+alamat[mer])
+                return
+
+            if "berhasil mencuri" in event.raw_text:
+                mer += 1
+                time.sleep(2)
+                await event.respond(''+alamat[mer])
+                return
+
+            if "Alamat yang sama" in event.raw_text:
+                mer += 1
+                time.sleep(2)
+                await event.respond(''+alamat[mer])
+                return
+
+            if "Keamanan rumah" in event.raw_text:
+                mer += 1
+                time.sleep(2)
+                await event.respond(''+alamat[mer])
+                return
+
+            if "Rumah yang kamu kunjungi" in event.raw_text:
+                mer += 1
+                time.sleep(2)
+                await event.respond(''+alamat[mer])
+                return
+
+            if "Uuuh rasanya" in event.raw_text:
+                time.sleep(2)
+                mer += 1
+                await event.respond(mese)
+                return
+
+            if alamat[mer] == alamat[-1]:
+                mer = 0
+                print('Selesai Semua')
+                time.sleep(3)
+                await event.respond(''+alamat[mer])
+                return
+            else:
+                time.sleep(2)
+                mer += 0
+                await event.respond(''==alamat[mer])
+            
+        clien.run_until_disconnected()
+        print(time.asctime(), '-', 'Stop')
+
+threading.Thread(target=Dexter).start()
+print(time.asctime(), '-', 'Start')
+
+#if abis == 2 :
+#    client.disconnect()
+#    clien.disconnect()
